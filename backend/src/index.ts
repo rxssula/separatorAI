952533f01b2bb5dc610cd2543e17859ad40d8a92
rxssula <rxssul.aitkali@gmail.com>
2024-07-23 23:06:23 +0500

    fix cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -29,7 +29,12 @@ const createRequiredDirectories = () => {
 connectDB();
 createRequiredDirectories();
 
-app.use(cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 app.use(logger);
 app.use(express.json());
 app.use("/api/v1/", globalRouter);
