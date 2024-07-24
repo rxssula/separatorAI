d13869676a02030ecc9cfe92c2f01b38ca839516
rxssula <rxssul.aitkali@gmail.com>
2024-07-25 00:45:17 +0500

    check pip list

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,20 +4,16 @@ import connectDB from "./db";
 import globalRouter from "./global-router";
 import cors from "cors";
 import path from "path";
-import fs from "fs"
+import fs from "fs";
 import { logger } from "./logger";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 const createRequiredDirectories = () => {
-  const directories = [
-    'routes',
-    'routes/s3',
-    'routes/s3/uploads'
-  ];
+  const directories = ["routes", "routes/s3", "routes/s3/uploads"];
 
-  directories.forEach(dir => {
+  directories.forEach((dir) => {
     const fullPath = path.join(__dirname, dir);
     if (!fs.existsSync(fullPath)) {
       fs.mkdirSync(fullPath, { recursive: true });
@@ -26,13 +22,23 @@ const createRequiredDirectories = () => {
   });
 };
 
+function logPipList() {
+  try {
+    const pipList = fs.readFileSync("/app/pip_list.txt", "utf8");
+    console.log("Installed Python packages:");
+    console.log(pipList);
+  } catch (error) {
+    console.error("Error reading pip list:", error);
+  }
+}
+
 connectDB();
 createRequiredDirectories();
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
   next();
 });
 app.use(logger);
@@ -45,4 +51,5 @@ app.get("/helloworld", (request, response) => {
 
 app.listen(PORT, () => {
   console.log(`Server runs at http://localhost:${PORT}`);
+  logPipList();
 });
